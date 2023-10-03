218bc5a5d1546cb7442db13f1bd124c6055e0466
emilinasanduleac <e.codreanu@students.clark.edu>
2023-10-03 12:43:04 -0500

    fixed the javascript and the inputs get calculated

--- src/grades.js
+++ src/grades.js
@@ -1,32 +1,19 @@
-// let grades = document.getElementById('grades');
-//
-// if (grades > 0 && grades <= 59){
-//     console.log(`I apologize but you got an F ${grades}`);
-// } else if (grades > 60 && grades <= 69){
-//     console.log(`You were able to pass with a D ${grades}`);
-// } else if (grades > 70 && grades <= 79){
-//     console.log(`Yey you were able to pass with a C ${grades}`);
-// } else if (grades > 80 && grades <= 89){
-//     console.log(`You got a B ${grades}`);
-// } else if (grades > 90 && grades <= 100){
-//     console.log(`Congrats you got an A ${grades}`)
-// }else {
-//     console.log('Please input all your scores on a scale of 0-100');
-// }
 
-const desiredInput = document.getElementById(desiredGrade);
-const currentInput = document.getElementById(currentGrade);
-const weightInput = document.getElementById(weightGrade);
+'use strict';
 
-
+const desiredInput = document.getElementById("desiredGrade");
+const currentInput = document.getElementById("currentGrade");
+const weightInput = document.getElementById("weightGrade");
+const neededDiv = document.getElementById("neededGrade");
 
 let calculateGrade = () => {
     let desired = Number(desiredInput.value);
-    let final = Number(weightInput.value) - 100;
-    let current = Number(currentInput);
-    let total = desired - final * current;
-    let needed = total / final;
-    // needed.innerText = `${needed.toFixed(2)}`;
+    let final = Number(weightInput.value) ; // Calculate the final exam weight as 100 - weightInput.value
+    let current = Number(currentInput.value);     // Get the value of the current grade input
+    let total = (desired - (100 - final)) * current;      // Calculate the total points needed
+    let needed = total / final;                   // Calculate the needed final exam score
+    neededDiv.innerText = `You need to score ${needed.toFixed(2)}% on the final exam.`;
 };
 
-//comit
+// const calculateButton = document.getElementById("calculateButton");
+// calculateButton.addEventListener("click", calculateGrade);
