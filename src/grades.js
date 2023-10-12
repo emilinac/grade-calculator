54e820c079c77a63627608ad3b717f35232b2b36
emilinasanduleac <e.codreanu@students.clark.edu>
2023-10-12 11:33:45 -0500

    final calculator

--- src/grades.js
+++ src/grades.js
@@ -10,10 +10,8 @@ let calculateGrade = () => {
     let desired = Number(desiredInput.value);
     let final = Number(weightInput.value) ; // Calculate the final exam weight as 100 - weightInput.value
     let current = Number(currentInput.value);     // Get the value of the current grade input
-    let total = (desired - (100 - final)) * current;      // Calculate the total points needed
-    let needed = total / final;                   // Calculate the needed final exam score
+    let total = (desired - ((1 -(final / 100)) * current));      // Calculate the total points needed
+    let needed = ((total / final) * 100);                   // Calculate the needed final exam score
     neededDiv.innerText = `You need to score ${needed.toFixed(2)}% on the final exam.`;
 };
 
-// const calculateButton = document.getElementById("calculateButton");
-// calculateButton.addEventListener("click", calculateGrade);
